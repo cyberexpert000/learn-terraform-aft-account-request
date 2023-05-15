a4f1a74e7cde3bb02657d0c772509b36e83329f0
cyberexpert000 <security@cloudsec.biz>
2023-05-15 15:54:04 +0800

    updating dev01

--- terraform/account-request_Dev_01.tf
+++ terraform/account-request_Dev_01.tf
@@ -8,7 +8,7 @@ module "Dev_account_01" {
     AccountEmail = "development.apac+dev01@gmail.com"
     AccountName  = "Dev-account-01"
     # Syntax for top-level OU
-    ManagedOrganizationalUnit = "Development"
+    ManagedOrganizationalUnit = "Development (ou-g1zl-xbeg7fml)"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
     SSOUserEmail     = "development.apac+dev01@gmail.com"
